89ed13f3cd1981945c02005f1d53c0bb476637fb
Jah-Wilson Teeba <teebawilson91@gmail.com>
2021-08-28 16:07:25 -0400

    Update TestProducer.java

--- twitter-producer/src/test/java/com/github/jwteeba/TestProducer.java
+++ twitter-producer/src/test/java/com/github/jwteeba/TestProducer.java
@@ -2,7 +2,7 @@ package com.github.jwteeba;
 
 public class TestProducer {
     public static void main(String[] args) {
-        TwitterProducer producer = new TwitterProducer("/home/blackpanther07/.config/twitter-configs.conf");
+        TwitterProducer producer = new TwitterProducer("/path/to/CONFIG");
         producer.run();
     }
 }
